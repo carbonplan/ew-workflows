8b86b6c5c6a9c1bb0bca8cf78a3985b541448acc
tykukla <tkukla13@gmail.com>
2025-05-01 18:51:17 -0500

    fix floating point issue in postprocessing

--- scripts/scepter/setup/batch/make_batch_input_grainsize+apprate.py
+++ scripts/scepter/setup/batch/make_batch_input_grainsize+apprate.py
@@ -25,9 +25,9 @@ import batch_helperFxns as bhf
 # %% 
 # --- USER INPUTS
 # [1] vars to update, constant for all runs
-fertLevel = "hi"    # name for how much fertilizer is applied
-dustsp = "gbas"      # name for dust species to apply (must be from accepted list)
-extra_tag = "test_v0"  # another distinguishing tag
+fertLevel = "low"    # name for how much fertilizer is applied
+dustsp = "cc"      # name for dust species to apply (must be from accepted list)
+extra_tag = "base_psdfull"  # another distinguishing tag
 pref = f"{fertLevel}Fert_{dustsp}_{extra_tag}"
 clim_tag = None   # [string] year-span (e.g., 1950-2020) for clim input if climate files are used
                   # (if clim files are not used, set to None)
@@ -37,28 +37,38 @@ fn = file_prefix + "_v0.csv"
 savepath_batch = "/home/tykukla/ew-workflows/inputs/scepter/batch"
 multi_run_split = False   # whether to split the csv into multiple files
 max_iters_per_set = 20    # [int] the number of runs per csv (only used if multi_run_split is True)
+# **************************
+# --- do not change
+fert_dict = {
+    "hi": 35.0,    # 30.0
+    "low": 3.0,    # 6.0
+    "no": 0.0,
+    }
+# **************************
 
 const_dict = {
-    "duration": 15,  # duration of run (starts from earliest year)
+    "duration": 15,  # [yr] duration of run (starts from earliest year)
     "dustsp": dustsp,
     "dustsp_2nd": "amnt",
-    "dustrate_2nd": 30.0, # 6.0, # 30., # 0.0, 
+    "dustrate_2nd": fert_dict[fertLevel],
     "add_secondary": False,
-    "imix": 3, # 1,
+    "imix": 3, # mixing style (1=fickian; 2=homogeneous; 3=tilling)
     "singlerun_seasonality": False,
-    "include_psd_full": False,   # False,
+    "include_psd_full": True,   # False,
     "include_psd_bulk": False,
     'poro_iter_field': False,      # [default='false'] porosity iteration
-    'poro_evol': False,            # [default='false'] porosity evolves with solid phase dissolution
+    'poro_evol': True,            # [default='false'] porosity evolves with solid phase dissolution
     "cec_adsorption_on": False, # True,
     "climatedir": "NA",
 
     # --- surface area and psd rules
     'sa_rule1': False,       # [True, False, "spinvalue"] SA decreases as porosity increases
     'sa_rule2': True,       # [True, False, "spinvalue"] SA increases as porosity increases
-    'psdrain_log10_sd': 0.2, # [] log 10 standard deviation for psd
+    'psdrain_log10_sd': 0.05, # [] log 10 standard deviation for psd
     'psdrain_wt': 1.0,       # [] weight for the psd
     'use_psdrain_datfile': False,  # False means we construct the PSD based on inputs, rather than copy an existing data file
+    'include_roughness_sa': True,  # [True, False] whether to apply roughness calculation from Navarre-Sitchler & Brantley, 2007 
+                                   # (roughness factor = (beta / a)^0.33) where beta is particle radius in m and a is BET measurement resolution in m, taken to be 10^-10 
     
     # # --- update CEC?
     # 'cec_update_from_spinup': True,   # [True, False] whether to update CEC and alpha vars relative to the spinup value (False means no change to cec.in is made)
@@ -91,10 +101,12 @@ by_site = {   # values must have same order as 'sites' var
 
 # %% 
 # [3] vars to vary within site (we'll get every combination of these two)
-dustrate_ton_ha_yr = [0.1, 0.3, 0.6, 1, 2, 5, 7, 10, 15, 25, 35, 45, 60, 100]
+# dustrate_ton_ha_yr = [0.1, 0.3, 0.6, 1, 2, 5, 7, 10, 15, 25, 35, 45, 60, 100]
+dustrate_ton_ha_yr = [0.1, 0.3, 0.6, 1, 1.5, 2, 4, 8, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 100]
+dustrad_um = [1, 3, 6, 10, 15, 20, 30, 40, 50, 60, 70, 80, 100, 125, 150, 175, 200, 250, 300]
 all_combinations = {
     "dustrate": [x * 100 for x in dustrate_ton_ha_yr],  # [ton ha-1 yr-1 * 100 = g m-2]   
-    "dustrad": [1, 10, 30, 50, 75, 100, 125, 150, 200, 300]  # [diameter, microns] i think this gets applied to gbas and amnt equally (though amnt is fast-reacting so maybe not a big deal? )
+    "dustrad": dustrad_um  # [diameter, microns] i think this gets applied to gbas and amnt equally (though amnt is fast-reacting so maybe not a big deal? )
 }
 
 
